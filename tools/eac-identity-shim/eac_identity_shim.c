442c5d78f0fd82a33e021b9b39e3c3136b018f5d
Avery Felts <alexmondello@Averys-MacBook-Air.local>
2026-06-06 11:12:24 -0600

    style: clang-format eac_identity_shim.c

--- tools/eac-identity-shim/eac_identity_shim.c
+++ tools/eac-identity-shim/eac_identity_shim.c
@@ -1,15 +1,19 @@
 #include <windows.h>
 
-__declspec(dllexport) const char *CDECL wine_get_version(void) { return NULL; }
+__declspec(dllexport) const char* CDECL wine_get_version(void) {
+    return NULL;
+}
 
-__declspec(dllexport) const char *CDECL wine_get_build_id(void) { return NULL; }
+__declspec(dllexport) const char* CDECL wine_get_build_id(void) {
+    return NULL;
+}
 
-__declspec(dllexport) const char *CDECL wine_get_unix_file_name(LPCWSTR dos) {
+__declspec(dllexport) const char* CDECL wine_get_unix_file_name(LPCWSTR dos) {
     (void)dos;
     return NULL;
 }
 
-__declspec(dllexport) const WCHAR *CDECL wine_get_dos_file_name(const char *unix_path) {
+__declspec(dllexport) const WCHAR* CDECL wine_get_dos_file_name(const char* unix_path) {
     (void)unix_path;
     return NULL;
 }
