09065698f1968f3916892ec256f88a0a04804bac
Avery Felts <alexmondello@Averys-MacBook-Air.local>
2026-06-07 18:20:50 -0600

    style: cargo fmt

--- app/src-rust/src/mtsp/engine.rs
+++ app/src-rust/src/mtsp/engine.rs
@@ -698,7 +698,14 @@ mod tests {
             .collect();
         assert_eq!(
             selectable,
-            vec![PipelineId::M12, PipelineId::M11, PipelineId::M10, PipelineId::M9, PipelineId::D3DMetal, PipelineId::FnaArm64]
+            vec![
+                PipelineId::M12,
+                PipelineId::M11,
+                PipelineId::M10,
+                PipelineId::M9,
+                PipelineId::D3DMetal,
+                PipelineId::FnaArm64
+            ]
         );
 
         let labels: Vec<_> = selectable.iter().map(|pipeline| pipeline.user_selectable_name().unwrap()).collect();
