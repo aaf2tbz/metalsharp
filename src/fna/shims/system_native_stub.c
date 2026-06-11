fb70b02eb6288af764db5839ab29e9746fab76f8
Avery Felts <alexmondello@Averys-MacBook-Air.local>
2026-06-11 04:54:45 -0600

    Fix FNA System.Native shim ABI

--- src/fna/shims/system_native_stub.c
+++ src/fna/shims/system_native_stub.c
@@ -13,101 +13,110 @@
 
 typedef struct {
     int32_t Flags;
-    int32_t Errno;
-    int32_t Dev;
-    int32_t _pad0;
-    int64_t Ino;
     int32_t Mode;
-    int32_t Uid;
-    int32_t Gid;
-    int32_t Rdev;
+    uint32_t Uid;
+    uint32_t Gid;
     int64_t Size;
-    int64_t AtimSec;
-    int64_t AtimNsec;
-    int64_t MtimSec;
-    int64_t MtimNsec;
-    int64_t CtimSec;
-    int64_t CtimNsec;
-    int64_t Blocks;
+    int64_t ATime;
+    int64_t ATimeNsec;
+    int64_t MTime;
+    int64_t MTimeNsec;
+    int64_t CTime;
+    int64_t CTimeNsec;
+    int64_t BirthTime;
+    int64_t BirthTimeNsec;
+    int64_t Dev;
+    int64_t Ino;
 } FileStatus;
 
-static void fill_file_status(FileStatus* fs, const struct stat* s, int32_t err) {
+typedef struct {
+    const char* Name;
+    int32_t NameLength;
+    int32_t InodeType;
+} DirectoryEntry;
+
+static void fill_file_status(FileStatus* fs, const struct stat* s) {
+    memset(fs, 0, sizeof(*fs));
     fs->Flags = 0;
-    fs->Errno = err;
-    if (err != 0)
-        return;
-    fs->Dev = (int32_t)s->st_dev;
-    fs->Ino = (int64_t)s->st_ino;
     fs->Mode = (int32_t)s->st_mode;
     fs->Uid = (uint32_t)s->st_uid;
     fs->Gid = (uint32_t)s->st_gid;
-    fs->Rdev = (int32_t)s->st_rdev;
     fs->Size = (int64_t)s->st_size;
-    fs->AtimSec = (int64_t)s->st_atimespec.tv_sec;
-    fs->AtimNsec = (int64_t)s->st_atimespec.tv_nsec;
-    fs->MtimSec = (int64_t)s->st_mtimespec.tv_sec;
-    fs->MtimNsec = (int64_t)s->st_mtimespec.tv_nsec;
-    fs->CtimSec = (int64_t)s->st_ctimespec.tv_sec;
-    fs->CtimNsec = (int64_t)s->st_ctimespec.tv_nsec;
-    fs->Blocks = (int64_t)s->st_blocks;
+    fs->ATime = (int64_t)s->st_atimespec.tv_sec;
+    fs->ATimeNsec = (int64_t)s->st_atimespec.tv_nsec;
+    fs->MTime = (int64_t)s->st_mtimespec.tv_sec;
+    fs->MTimeNsec = (int64_t)s->st_mtimespec.tv_nsec;
+    fs->CTime = (int64_t)s->st_ctimespec.tv_sec;
+    fs->CTimeNsec = (int64_t)s->st_ctimespec.tv_nsec;
+    fs->BirthTime = (int64_t)s->st_birthtimespec.tv_sec;
+    fs->BirthTimeNsec = (int64_t)s->st_birthtimespec.tv_nsec;
+    fs->Dev = (int64_t)s->st_dev;
+    fs->Ino = (int64_t)s->st_ino;
 }
 
 int32_t SystemNative_LChflagsCanSetHiddenFlag(void) {
     return 0;
 }
 
-int32_t SystemNative_Stat(const char* path, int32_t* len, struct stat* buf) {
-    (void)len;
-    if (stat(path, buf) != 0) {
-        return errno;
-    }
-    return 0;
+void SystemNative_GetNonCryptographicallySecureRandomBytes(uint8_t* buffer, int32_t length) {
+    arc4random_buf(buffer, (size_t)length);
 }
 
-int32_t SystemNative_Stat2(const char* path, int32_t* len, FileStatus* fs) {
-    (void)len;
+int32_t SystemNative_ConvertErrorPlatformToPal(int32_t platformErrno) {
+    return platformErrno;
+}
+
+int32_t SystemNative_Stat(const char* path, FileStatus* fs) {
     struct stat s;
     if (stat(path, &s) != 0) {
-        fill_file_status(fs, &s, errno);
-        return errno;
+        return -1;
     }
-    fill_file_status(fs, &s, 0);
+    fill_file_status(fs, &s);
     return 0;
 }
 
-int32_t SystemNative_LStat(const char* path, int32_t* len, struct stat* buf) {
-    (void)len;
-    if (lstat(path, buf) != 0) {
-        return errno;
+int32_t SystemNative_Stat2(const char* path, FileStatus* fs) {
+    struct stat s;
+    if (stat(path, &s) != 0) {
+        return -1;
     }
+    fill_file_status(fs, &s);
     return 0;
 }
 
-int32_t SystemNative_LStat2(const char* path, int32_t* len, FileStatus* fs) {
-    (void)len;
+int32_t SystemNative_LStat(const char* path, FileStatus* fs) {
     struct stat s;
     if (lstat(path, &s) != 0) {
-        fill_file_status(fs, &s, errno);
-        return errno;
+        return -1;
     }
-    fill_file_status(fs, &s, 0);
+    fill_file_status(fs, &s);
     return 0;
 }
 
-int32_t SystemNative_FStat(int32_t fd, struct stat* buf) {
-    if (fstat(fd, buf) != 0) {
-        return errno;
-    }
-    return 0;
-}
-
-int32_t SystemNative_FStat2(int32_t fd, FileStatus* fs) {
+int32_t SystemNative_LStat2(const char* path, FileStatus* fs) {
     struct stat s;
-    if (fstat(fd, &s) != 0) {
-        fill_file_status(fs, &s, errno);
-        return errno;
+    if (lstat(path, &s) != 0) {
+        return -1;
     }
-    fill_file_status(fs, &s, 0);
+    fill_file_status(fs, &s);
+    return 0;
+}
+
+int32_t SystemNative_FStat(intptr_t fd, FileStatus* fs) {
+    struct stat s;
+    if (fstat((int)fd, &s) != 0) {
+        return -1;
+    }
+    fill_file_status(fs, &s);
+    return 0;
+}
+
+int32_t SystemNative_FStat2(intptr_t fd, FileStatus* fs) {
+    struct stat s;
+    if (fstat((int)fd, &s) != 0) {
+        return -1;
+    }
+    fill_file_status(fs, &s);
     return 0;
 }
 
@@ -139,13 +148,12 @@ int32_t SystemNative_Symlink(const char* target, const char* linkpath) {
     return 0;
 }
 
-int32_t SystemNative_ReadLink(const char* path, char* buf, int32_t bufSize, int32_t* written) {
+int32_t SystemNative_ReadLink(const char* path, char* buf, int32_t bufSize) {
     ssize_t r = readlink(path, buf, (size_t)bufSize);
     if (r < 0) {
-        return errno;
+        return -1;
     }
-    *written = (int32_t)r;
-    return 0;
+    return (int32_t)r;
 }
 
 int32_t SystemNative_Unlink(const char* path) {
@@ -209,6 +217,46 @@ int32_t SystemNative_TruncateFile(int32_t fd, int64_t length) {
     return 0;
 }
 
+int32_t SystemNative_GetReadDirRBufferSize(void) {
+    return (int32_t)sizeof(struct dirent);
+}
+
+int32_t SystemNative_ReadDirR(DIR* dir, void* buffer, int32_t bufferSize, DirectoryEntry* outputEntry) {
+    if (dir == NULL || buffer == NULL || outputEntry == NULL || bufferSize < (int32_t)sizeof(struct dirent)) {
+        errno = EINVAL;
+        return EINVAL;
+    }
+
+    errno = 0;
+    struct dirent* entry = readdir(dir);
+    if (entry == NULL) {
+        memset(outputEntry, 0, sizeof(*outputEntry));
+        return errno == 0 ? -1 : errno;
+    }
+
+    size_t reclen = entry->d_reclen;
+    if (reclen > (size_t)bufferSize) {
+        errno = ERANGE;
+        memset(outputEntry, 0, sizeof(*outputEntry));
+        return ERANGE;
+    }
+
+    memcpy(buffer, entry, reclen);
+    struct dirent* copied = (struct dirent*)buffer;
+    outputEntry->Name = copied->d_name;
+    outputEntry->NameLength = copied->d_namlen;
+    outputEntry->InodeType = copied->d_type;
+    return 0;
+}
+
+DIR* SystemNative_OpenDir(const char* path) {
+    return opendir(path);
+}
+
+int32_t SystemNative_CloseDir(DIR* dir) {
+    return closedir(dir);
+}
+
 int32_t SystemNative_GetReadLinkSymlinkMaximumSize(void) {
     return 1024;
 }
@@ -216,7 +264,7 @@ int32_t SystemNative_GetReadLinkSymlinkMaximumSize(void) {
 int32_t SystemNative_Open(const char* path, int32_t flags, int32_t mode) {
     int fd = open(path, flags, (mode_t)mode);
     if (fd < 0) {
-        return -errno;
+        return -1;
     }
     return fd;
 }
@@ -231,7 +279,7 @@ int32_t SystemNative_Close(int32_t fd) {
 int64_t SystemNative_LSeek(int32_t fd, int64_t offset, int32_t whence) {
     off_t r = lseek(fd, offset, whence);
     if (r < 0) {
-        return -errno;
+        return -1;
     }
     return r;
 }
